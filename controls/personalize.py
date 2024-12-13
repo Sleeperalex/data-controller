aaec9eba1d3ef6301af723987a04e46562ba7ea2
alex <131412582+Sleeperalex@users.noreply.github.com>
2024-12-14 00:52:18 +0100

    Delete controls directory

--- controls/personalize.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-@st.cache_data
-def check_regex(df, regex_pattern, column_name):
-    if column_name in df.columns:
-        if not df[column_name].str.match(regex_pattern).all():
-            return False
-    return True
